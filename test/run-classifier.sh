0945bb9e54a79c7a013a6f6b4c26bb87b078d20f
Matheus Castanho <mscastanho@gmail.com>
2018-09-13 10:44:03 -0300

    Added uBPF VM code from IOVisor

--- test/run-classifier.sh
+++ test/run-classifier.sh
@@ -1,3 +1,3 @@
 cd $(dirname "$0")
-../build/sfcapp -c 0x2 -n 2 -m 1024 -- -p 3 -t classifier -f ../config/classifier.cfg
+../sfc/build/sfcapp -c 0x2 -n 2 -m 1024 -- -p 3 -t classifier -f ../config/classifier.cfg
 cd -
--- test/run-forwarder.sh
+++ test/run-forwarder.sh
@@ -1,3 +1,3 @@
 cd $(dirname "$0")
-../build/sfcapp -c 0x2 -n 2 -m 4096 -- -p 3 -t forwarder -f ../config/forwarder.cfg
+../sfc/build/sfcapp -c 0x2 -n 2 -m 4096 -- -p 3 -t forwarder -f ../config/forwarder.cfg
 cd -
