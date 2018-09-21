d0de2d2a31208b0eaf88b29bea97417069b2303e
Matheus Castanho <mscastanho@gmail.com>
2018-09-21 17:52:15 -0300

    Changed export-vars.sh file to use v18.08

--- config/export-vars.sh
+++ config/export-vars.sh
@@ -1,4 +1,4 @@
 export OVS_DIR=/home/user/openvswitch-2.8.0
 export RTE_TARGET=x86_64-native-linuxapp-gcc
-export RTE_SDK=/home/user/dpdk-17.05.2
+export RTE_SDK=/home/user/dpdk-18.08
 export VM_DIR=/home/user
